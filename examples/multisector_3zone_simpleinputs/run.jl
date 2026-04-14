a2c66247e3a02e7746f2d2102131e12d25b5a1a4
Ruaridh Macdonald <rmacd@mit.edu>
2026-04-14 06:48:05 -0400

    Update multisector_3zone_simpleinputs with new supply and Node BalanceConstraint inputs

--- examples/multisector_3zone_simpleinputs/run.jl
+++ examples/multisector_3zone_simpleinputs/run.jl
@@ -1,9 +1,7 @@
 using MacroEnergy
 using Gurobi
 
-(system, model) = run_case(
-    @__DIR__;
-    optimizer=Gurobi.Optimizer,
-    optimizer_attributes=("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-3),
+(system, model) = run_case(@__DIR__; 
+    optimizer=HiGHS.Optimizer,
+    optimizer_attributes=("solver" => "ipm", "run_crossover" => "off", "ipm_optimality_tolerance" => 1e-3)
 );
-
