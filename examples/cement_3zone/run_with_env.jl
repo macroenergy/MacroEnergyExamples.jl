8d5beae23d8a7b7aa15424d155c95a2ddbbae211
lbonaldo <39280783+lbonaldo@users.noreply.github.com>
2026-05-15 15:22:12 -0400

    Update examples with new return variables from run_case
    This commit updates the examples following the merge of PR
    #239 of MacroEnergy. The `run_case` function now returns the
    full `Case` object instead of the vector of systems.

--- examples/cement_3zone/run_with_env.jl
+++ examples/cement_3zone/run_with_env.jl
@@ -5,4 +5,4 @@ if !(@isdefined GRB_ENV)
     const GRB_ENV = Gurobi.Env()
 end
 
-(system, model) = run_case(@__DIR__; optimizer=Gurobi.Optimizer, optimizer_env=GRB_ENV);
+(case, solution) = run_case(@__DIR__; optimizer=Gurobi.Optimizer, optimizer_env=GRB_ENV);
